6465fb80621c3ff8c3b04572d82bc5a8929cea1e
HTMLDigger <htmldigger@gmail.com>
2020-05-31 18:52:28 +0530

    just test file.

---
+++ setenv.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+# Setenv v1.0
+# Powered by HTMLDigger
+# Credits goes to HTMLdigger
+
+trap 'printf "\n";stop' 2
+
+banner() {
+clear
+printf "\e[1;92m  ╦ ╦╔╦╗╔╦╗╦  ╔╦╗┬┌─┐┌─┐┌─┐┬─┐   \e[0m\n"
+printf "\e[1;92m  ╠═╣ ║ ║║║║   ║║││ ┬│ ┬├┤ ├┬┘\e[0m\n"
+printf "\e[1;92m |╩ ╩ ╩ ╩ ╩╩═╝═╩╝┴└─┘└─┘└─┘┴└─\e[0m\n"
+
+printf " \e[1;77m www.htmldigger.in \e[0m \n"
+
+printf "\n"
