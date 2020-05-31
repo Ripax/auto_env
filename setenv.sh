d945e5886afc0646e035070852b4d037ec6138db
HTMLDigger <htmldigger@gmail.com>
2020-05-31 20:44:50 +0530

    update

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 # Setenv v1.0
 # Powered by HTMLDigger
@@ -13,5 +12,21 @@ printf "\e[1;92m  ╠═╣ ║ ║║║║   ║║││ ┬│ ┬├┤ ├
 printf "\e[1;92m |╩ ╩ ╩ ╩ ╩╩═╝═╩╝┴└─┘└─┘└─┘┴└─\e[0m\n"
 
 printf " \e[1;77m www.htmldigger.in \e[0m \n"
+}
 
-printf "\n"
+#Fail safe for original user sources.list in case setup was interrupted in middle last time
+file="/home/rion/MyWorks/bash/bin/duck"
+if [ -f "$file" ]
+then
+echo "Setup Detected that your previous run was interrupted in middle , fixing your original repositories list ."
+sleep 4s
+# rm -f /etc/apt/sources.list
+# mv /etc/apt/sources.list.fatrat /etc/apt/sources.list
+echo "Your Original repository list was recovered. ;) ..... beginning setup"
+echo ""
+echo "Cleaning previous repositories cache & updating your repository ."
+# sudo apt-get clean && apt-get update -y
+sleep 3s
+else
+echo "else command."
+fi 
